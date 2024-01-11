79bc0dcb37ba79f7b23fee307f254ac02e04710f
Sylwia Rumińska <sbwinga@gmail.com>
2024-01-11 22:32:30 +0100

    Challenge day 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var age = 30;
+var sex = "kobieta";
+
+if (age < 30 && sex == "kobieta")
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+
+else if (name == "Ewa" && age == 30 && sex == "kobieta")
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (age < 18 && sex == "mężczyzna")
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
